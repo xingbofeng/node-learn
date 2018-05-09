5f398c07a1eced5830ce178bcbd16159363f3b98
xingbofeng <503908971@qq.com>
2018-05-09 21:13:08 +0800

    add node C++

---
+++ C++/hello.cc
@@ -0,0 +1,30 @@
+// hello.cc
+#include <node.h>
+
+namespace demo {
+
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Object;
+using v8::String;
+using v8::Value;
+
+void World(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+}
+
+void Hello(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "hello"));
+}
+
+void init(Local<Object> exports) {
+  NODE_SET_METHOD(exports, "hello", Hello);
+  NODE_SET_METHOD(exports, "world", World);
+}
+
+NODE_MODULE(NODE_GYP_MODULE_NAME, init)
+
+}  // namespace demo
---
+++ callback/spider.js
@@ -0,0 +1,9 @@
+const request = require('request');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const utils = require('./utils');
+
+function spider() {
+  const filename = utils.urlToFilename(url);
+}
